236cc1b57266692d8562ed79e87ac3b189518150
Sam Jensen <samual.jensen@gmail.com>
2023-03-09 17:38:48 -0800

    added to Thought.js model

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types} = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        _id: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.objectId(),
+        },
+        thoughtId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.objectId(),
+        },
+        reaction: {
+            type: String,
+            required: true,
+            maxLength: 300,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 300,
+        },
+        createdAt: {
+            type: Date,
+            deafult: Date.now
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
